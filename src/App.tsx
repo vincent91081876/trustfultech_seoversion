29607a8933c4723bd4b845ba70e286d3a2827093
vincent91081876 <65351167+vincent91081876@users.noreply.github.com>
2026-07-13 16:43:38 +0800

    認可

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
